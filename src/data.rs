b78fe5657ab2b169d2412e2f0afcc5e1bec63694
Wim Looman <wim@nemo157.com>
2016-10-28 21:15:33 +0200

    Parse returned multiaddrs

--- src/data.rs
+++ src/data.rs
@@ -1,3 +1,10 @@
+use std::error::Error as StdError;
+use std::str::FromStr;
+
+use serde;
+use serde::de::{ self, Deserializer };
+use multiaddr::MultiAddr;
+
 #[derive(Debug, Deserialize)]
 pub struct PeerInfo {
     #[serde(rename = "ID")]
@@ -7,7 +14,8 @@ pub struct PeerInfo {
     pub public_key: String,
 
     #[serde(rename = "Addresses")]
-    pub addresses: Vec<String>,
+    #[serde(deserialize_with = "from_strs")]
+    pub addresses: Vec<MultiAddr>,
 
     #[serde(rename = "AgentVersion")]
     pub agent_version: String,
@@ -29,9 +37,44 @@ pub struct Version {
 }
 
 pub mod swarm {
+    use multiaddr::MultiAddr;
+    use super::from_strs;
+
     #[derive(Debug, Deserialize)]
     pub struct Peers {
         #[serde(rename = "Strings")]
-        pub addresses: Vec<String>,
+        #[serde(deserialize_with = "from_strs")]
+        pub addresses: Vec<MultiAddr>,
     }
 }
+
+fn from_strs<T, D>(d: &mut D) -> Result<Vec<T>, D::Error>
+    where T: FromStr,
+          <T as FromStr>::Err: StdError,
+          D: Deserializer
+{
+    let strs = try!(d.deserialize_seq(de::impls::VecVisitor::<String>::new()));
+    let mut result = Vec::with_capacity(strs.len());
+    for s in strs {
+        result.push(try!(s.parse().map_err(|e| <D::Error as serde::Error>::custom(<<T as FromStr>::Err as StdError>::description(&e)))));
+    }
+    Ok(result)
+}
+
+// fn from_str<T, D>(d: &mut D) -> Result<T, D::Error>
+//     where T: FromStr,
+//           <T as FromStr>::Err: StdError,
+//           D: Deserializer
+// {
+//     struct Visitor;
+// 
+//     impl de::Visitor for Visitor {
+//         type Value = String;
+// 
+//         fn visit_str<E: de::Error>(&mut self, value: &str) -> Result<String, E> {
+//             Ok(value.to_owned())
+//         }
+//     }
+// 
+//     Ok(try!(try!(d.deserialize_str(Visitor)).parse().map_err(|e| <D::Error as serde::Error>::custom(<<T as FromStr>::Err as StdError>::description(&e)))))
+// }
